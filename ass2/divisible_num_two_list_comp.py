945d221397592af2061834cc06800933fd5353c2
Rahul Garg <vihu89@gmail.com>
2015-11-22 23:46:03 -0800

    finally done. thank tycho

--- ass2/divisible_num_two_list_comp.py
+++ ass2/divisible_num_two_list_comp.py
@@ -13,7 +13,8 @@ import unittest
 def divisible_numbers(a_list, a_list_of_terms):
     # use 2 list comps
     # return [num for num in a_list if num%([a_list_of_terms[0]*k for k in a_list_of_terms[1:]][0]) == 0]
-    pass
+    # tycho --> [x for x in a_list if [x for y in a_list_of_terms if x % y == 0].count(x) == len(a_list_of_terms)
+    return [x for x in a_list if len([x for y in a_list_of_terms if x % y == 0]) == len(a_list_of_terms)]
 
 
 class DivisibleNumbersTestCase(unittest.TestCase):
