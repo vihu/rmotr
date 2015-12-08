d7681b9dfde38bd6ad30ccfab070767092769696
Rahul Garg <rahgarg@cisco.com>
2015-12-08 15:25:41 -0800

    done with ass2

--- ass2/func_factorial.py
+++ ass2/func_factorial.py
@@ -1,5 +1,6 @@
 """
-The objective of this assignment is to write a factorial function (https://en.wikipedia.org/wiki/Factorial).
+The objective of this assignment is to write a factorial function
+(https://en.wikipedia.org/wiki/Factorial).
 
 In order to do that we'll define 2 helper functions:
  * factorial_terms: will receive a number and return the
@@ -7,10 +8,12 @@ In order to do that we'll define 2 helper functions:
  * compute_factorial: Will receive a list of terms and compute de factorial.
                                 *You must use the `reduce` function*
 
-Finally, the `factorial` function will get a number and using both helper functions will compute
-the answer. The `factorial` function can have just 1 line.
+Finally, the `factorial` function will get a number and using both helper
+functions will compute the answer. The `factorial` function can have just
+1 line.
 
-Use the tests to see the complete specification of what you're required to write.
+Use the tests to see the complete specification of what you're required to
+write.
 
 """
 import unittest
